3bb2206cc06abd65b40baf7f22bafba4f85805cf
Eallison123 <42753797+Eallison123@users.noreply.github.com>
2021-04-30 14:09:32 -0700

    Refactored code to function better and be easier to read
    changed var to let and const, added use strict, combined conditionals into main loop

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
-var args = process.argv.slice(2)
-var numbersInWords = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-for (var i = 0; i < args.length; i ++){
-    var currentNumber = args[i]
-    var output = ""
-    var negative = "" //assuming negative numbers are a valid input
-    if (currentNumber == 0){
-        output = "Zero"
-    }
+'use strict';
+const args = process.argv.slice(2)
+const numbersInWords = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+for (let i = 0; i < args.length; i ++){
+    let currentNumber = args[i]
+    let output = ""
+    let negative = "" //assuming negative numbers are a valid input
+
     if (currentNumber < 0){
         currentNumber = currentNumber * -1
         negative = "Negative"
     }
+
     //loops through each number adding the number in the ones column, then dividing by 10 and removing the decimal
-    while (currentNumber > 0){ 
+    do{ 
         output = numbersInWords[currentNumber % 10] + output//i.e. 256 -> 6
         currentNumber = Math.floor(currentNumber / 10);//i.e. 256 -> 25.6 -> 25
-    }
-    if (i == args.length - 1){
-        process.stdout.write(negative + output)
-        break
-    }
-    process.stdout.write(negative + output + ",")
-    output = ""
-    negative = ""
-}
+    } while (currentNumber > 0)
 
+    if (i < args.length - 1){
+        output += ","
+    }
+    process.stdout.write(negative + output)
+}
